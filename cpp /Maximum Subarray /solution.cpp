43a05f484fd8e899a4c077f43342651a830a6699
Aashish Deshpande <167343488+ashishpdeshpande@users.noreply.github.com>
2024-12-17 22:38:25 +0530

    Update solution.cpp

--- cpp /Maximum Subarray /solution.cpp
+++ cpp /Maximum Subarray /solution.cpp
@@ -1,1 +1,33 @@
-....
+// 1> Brute Force Approch 
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        for(int start = 0; start < nums.size(); start++) {
+            int currentSum = 0;
+            for(int end = start; end < nums.size(); end++) {
+                currentSum += nums[end];
+                maxSum = max(maxSum , currentSum);
+            }
+        }
+        return maxSum;
+    }
+};
+
+// 2> Kadane's Algorithm
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        // Kadane's Algorithm
+        int maxSum = INT_MIN;
+        int currentSum = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            currentSum += nums[i];
+            maxSum = max(currentSum , maxSum);
+            if(currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
